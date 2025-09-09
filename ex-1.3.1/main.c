bbf7ecb6bac127ed463d30114edaa77b6b13375c
davidpegoral <davidcpegoral@gmail.com>
2025-09-09 10:25:38 -0300

    Create main.c

---
+++ ex-1.3.1/main.c
@@ -0,0 +1,33 @@
+#include <SDL2/SDL.h>
+
+int main (int argc, char* args[])
+{
+    /* INICIALIZACAO */
+    SDL_Init(SDL_INIT_EVERYTHING);
+    SDL_Window* win = SDL_CreateWindow("Animacao Simples",
+                         SDL_WINDOWPOS_UNDEFINED,
+                         SDL_WINDOWPOS_UNDEFINED,
+                         200, 100, SDL_WINDOW_SHOWN
+                      );
+    SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
+
+    /* EXECUÇÃO */
+    SDL_Rect r = { 95,-8, 10,10 };
+    while (r.y < 200) {
+        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+        SDL_RenderClear(ren);
+        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+        SDL_RenderFillRect(ren, &r);
+        SDL_RenderPresent(ren);
+        SDL_Delay(200);
+        r.y += 2;
+        if(r.y == 100){
+            r.y = -8;
+        }
+    }
+
+    /* FINALIZACAO */
+    SDL_DestroyRenderer(ren);
+    SDL_DestroyWindow(win);
+    SDL_Quit();
+}
